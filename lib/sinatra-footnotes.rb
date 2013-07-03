6447592cfda2b071d2df4211409296d936eeb2b3
Daniel Stutzman <dtstutz@gmail.com>
2013-07-02 20:53:42 -0600

    Refactored instance variable addition to helper method.

--- lib/sinatra-footnotes.rb
+++ lib/sinatra-footnotes.rb
@@ -14,16 +14,18 @@ module Sinatra
     ]
 
     module Helpers
+      def html_for_instance_variables
+        ivars = instance_variables - IGNORED_IVARS.map { |ivar| ivar.intern }
+        list_items = ivars.map { |ivar| "<li>#{ivar}</li>\n" }
+        (["<hr>", "Instance vars", "<ul>"] + list_items + ["</ul>"]).join("\n")
+      end
     end
 
     def self.registered(app)
       app.helpers Footnotes::Helpers
 
       app.after do
-        ivars = instance_variables - IGNORED_IVARS.map { |ivar| ivar.intern }
-        list_items = ivars.map { |ivar| "<li>#{ivar}</li>\n" }
-        response.body +=
-          ["<hr>\nInstance vars\n<ul>\n#{list_items.join}</ul>\n"]
+        response.body += [html_for_instance_variables]
       end
     end
   end
