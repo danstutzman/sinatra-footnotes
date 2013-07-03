58265db510a9691f0800e53a2374ef629a7a74ed
Daniel Stutzman <dtstutz@gmail.com>
2013-07-02 21:07:53 -0600

    Show values of instance variables; use tables instead of unordered lists.

--- lib/sinatra-footnotes.rb
+++ lib/sinatra-footnotes.rb
@@ -16,19 +16,37 @@ module Sinatra
     module Helpers
       def html_for_instance_variables
         ivars = instance_variables - IGNORED_IVARS.map { |ivar| ivar.intern }
-        list_items = ivars.map { |ivar| "<li>#{ivar}</li>\n" }
-        (["<hr>", "Instance vars", "<ul>"] + list_items + ["</ul>"]).join("\n")
+
+        htmls = []
+        htmls.push "<hr>"
+        htmls.push "Instance vars"
+        htmls.push "<table>"
+        htmls.push "<tr><th>Name</th><th>Value</th></tr>"
+        ivars.each do |ivar|
+          htmls.push "<tr>"
+          htmls.push "<td>#{ivar}</td>"
+          htmls.push "<td>#{instance_variable_get(ivar).inspect}</td>"
+          htmls.push "</tr>"
+        end
+        htmls.push "</table>"
+
+        htmls.join("\n")
       end
 
       def html_for_params
         htmls = []
         htmls.push "<hr>"
         htmls.push "Params:"
-        htmls.push "<ul>"
+        htmls.push "<table>"
+        htmls.push "<tr><th>Name</th><th>Value</th></tr>"
         params.each do |name, value|
-          htmls.push "<li>#{name}=#{value}</li>"
+          htmls.push "<tr>"
+          htmls.push "<td>#{name.intern.inspect}</td>"
+          htmls.push "<td>#{value.inspect}</td>"
+          htmls.push "</tr>"
         end
-        htmls.push "</ul>"
+        htmls.push "</table>"
+
         htmls.join("\n")
       end
     end
