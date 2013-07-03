64cc0849f9f47127546062fd7ffa8ca040a0c46a
Daniel Stutzman <dtstutz@gmail.com>
2013-07-02 20:51:05 -0600

    Show instance variables at the end of the page.

--- lib/sinatra-footnotes.rb
+++ lib/sinatra-footnotes.rb
@@ -1,7 +1,32 @@
-require "sinatra-footnotes/version"
+require 'sinatra-footnotes/version'
 require 'sinatra/base'
 
 module Sinatra
   module Footnotes
+    IGNORED_IVARS = %w[
+      @default_layout
+      @app
+      @template_cache
+      @env
+      @request
+      @response
+      @params
+    ]
+
+    module Helpers
+    end
+
+    def self.registered(app)
+      app.helpers Footnotes::Helpers
+
+      app.after do
+        ivars = instance_variables - IGNORED_IVARS.map { |ivar| ivar.intern }
+        list_items = ivars.map { |ivar| "<li>#{ivar}</li>\n" }
+        response.body +=
+          ["<hr>\nInstance vars\n<ul>\n#{list_items.join}</ul>\n"]
+      end
+    end
   end
+
+  register Footnotes
 end
